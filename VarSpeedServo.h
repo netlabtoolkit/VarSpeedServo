f70fb7d011f02b2da836355edc79f04699b1e1a6
sapics <gv.nishino@gmail.com>
2019-10-10 11:07:53 +0900

    Fix comment of write method

--- VarSpeedServo.h
+++ VarSpeedServo.h
@@ -152,7 +152,7 @@ public:
   uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
   uint8_t attach(int pin, int min, int max); // as above but also sets min and max values for writes.
   void detach();
-  void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds
+  void write(int value);             // if value is < 544 its treated as an angle, otherwise as pulse width in microseconds
   void write(int value, uint8_t speed); // Move to given position at reduced speed.
           // speed=0 is identical to write, speed=1 slowest and speed=255 fastest.
           // On the RC-Servos tested, speeds differences above 127 can't be noticed,
