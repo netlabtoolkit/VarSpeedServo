da68014a1387084b87ac024a9365d07b717663b0
sapics <gv.nishino@gmail.com>
2019-10-11 15:14:20 +0900

    Add attach(pin, min, max, initialPulseWidth)
    We can control initial move of servo motor with initialPulseWidth.

--- VarSpeedServo.h
+++ VarSpeedServo.h
@@ -151,6 +151,8 @@ public:
   VarSpeedServo();
   uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
   uint8_t attach(int pin, int min, int max); // as above but also sets min and max values for writes.
+  uint8_t attach(int pin, int min, int max, int initialPulseWidth); // as above but also sets initial pulse width.
+          // If you set 0, servo dose not move from initial position.
   void detach();
   void write(int value);             // if value is < 544 its treated as an angle, otherwise as pulse width in microseconds
   void write(int value, uint8_t speed); // Move to given position at reduced speed.
